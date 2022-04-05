d350cf8eb10424eef2348773e7d6fe4f12512e21
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-04 21:22:27 -0600

    Add project files.

---
+++ Domain/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Domain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbcity = new System.Windows.Forms.Label();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.lblcondicion = new System.Windows.Forms.Label();
+            this.lbldetalles = new System.Windows.Forms.Label();
+            this.lblamanecer = new System.Windows.Forms.Label();
+            this.lblsunrise = new System.Windows.Forms.Label();
+            this.lblsunset = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblpressure = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblwindspeed = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbcity
+            // 
+            this.lbcity.AutoSize = true;
+            this.lbcity.BackColor = System.Drawing.Color.Transparent;
+            this.lbcity.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbcity.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lbcity.Location = new System.Drawing.Point(49, 71);
+            this.lbcity.Name = "lbcity";
+            this.lbcity.Size = new System.Drawing.Size(30, 13);
+            this.lbcity.TabIndex = 0;
+            this.lbcity.Text = "City";
+            // 
+            // txtcity
+            // 
+            this.txtcity.Location = new System.Drawing.Point(107, 64);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(100, 20);
+            this.txtcity.TabIndex = 1;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.BackColor = System.Drawing.Color.Transparent;
+            this.btnbuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnbuscar.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbuscar.ForeColor = System.Drawing.Color.White;
+            this.btnbuscar.Location = new System.Drawing.Point(250, 62);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 2;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = false;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // lblcondicion
+            // 
+            this.lblcondicion.AutoSize = true;
+            this.lblcondicion.BackColor = System.Drawing.Color.Transparent;
+            this.lblcondicion.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcondicion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblcondicion.Location = new System.Drawing.Point(36, 211);
+            this.lblcondicion.Name = "lblcondicion";
+            this.lblcondicion.Size = new System.Drawing.Size(61, 13);
+            this.lblcondicion.TabIndex = 3;
+            this.lblcondicion.Text = "Condicion";
+            this.lblcondicion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbldetalles
+            // 
+            this.lbldetalles.AutoSize = true;
+            this.lbldetalles.BackColor = System.Drawing.Color.Transparent;
+            this.lbldetalles.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldetalles.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lbldetalles.Location = new System.Drawing.Point(47, 258);
+            this.lbldetalles.Name = "lbldetalles";
+            this.lbldetalles.Size = new System.Drawing.Size(50, 13);
+            this.lbldetalles.TabIndex = 4;
+            this.lbldetalles.Text = "Detalles";
+            this.lbldetalles.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblamanecer
+            // 
+            this.lblamanecer.AutoSize = true;
+            this.lblamanecer.BackColor = System.Drawing.Color.Transparent;
+            this.lblamanecer.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblamanecer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblamanecer.Location = new System.Drawing.Point(30, 311);
+            this.lblamanecer.Name = "lblamanecer";
+            this.lblamanecer.Size = new System.Drawing.Size(60, 13);
+            this.lblamanecer.TabIndex = 5;
+            this.lblamanecer.Text = "Amanecer";
+            this.lblamanecer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblsunrise
+            // 
+            this.lblsunrise.AutoSize = true;
+            this.lblsunrise.BackColor = System.Drawing.Color.Transparent;
+            this.lblsunrise.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsunrise.ForeColor = System.Drawing.Color.White;
+            this.lblsunrise.Location = new System.Drawing.Point(102, 311);
+            this.lblsunrise.Name = "lblsunrise";
+            this.lblsunrise.Size = new System.Drawing.Size(26, 13);
+            this.lblsunrise.TabIndex = 6;
+            this.lblsunrise.Text = "N/A";
+            this.lblsunrise.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblsunset
+            // 
+            this.lblsunset.AutoSize = true;
+            this.lblsunset.BackColor = System.Drawing.Color.Transparent;
+            this.lblsunset.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsunset.ForeColor = System.Drawing.Color.White;
+            this.lblsunset.Location = new System.Drawing.Point(102, 368);
+            this.lblsunset.Name = "lblsunset";
+            this.lblsunset.Size = new System.Drawing.Size(26, 13);
+            this.lblsunset.TabIndex = 8;
+            this.lblsunset.Text = "N/A";
+            this.lblsunset.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(36, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sunset";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblpressure
+            // 
+            this.lblpressure.AutoSize = true;
+            this.lblpressure.BackColor = System.Drawing.Color.Transparent;
+            this.lblpressure.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpressure.ForeColor = System.Drawing.Color.White;
+            this.lblpressure.Location = new System.Drawing.Point(337, 281);
+            this.lblpressure.Name = "lblpressure";
+            this.lblpressure.Size = new System.Drawing.Size(26, 13);
+            this.lblpressure.TabIndex = 12;
+            this.lblpressure.Text = "N/A";
+            this.lblpressure.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(271, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 11;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblwindspeed
+            // 
+            this.lblwindspeed.AutoSize = true;
+            this.lblwindspeed.BackColor = System.Drawing.Color.Transparent;
+            this.lblwindspeed.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwindspeed.ForeColor = System.Drawing.Color.White;
+            this.lblwindspeed.Location = new System.Drawing.Point(337, 224);
+            this.lblwindspeed.Name = "lblwindspeed";
+            this.lblwindspeed.Size = new System.Drawing.Size(26, 13);
+            this.lblwindspeed.TabIndex = 10;
+            this.lblwindspeed.Text = "N/A";
+            this.lblwindspeed.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(265, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Wind Speed";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(265, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = " ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // picIcon
+            // 
+            this.picIcon.BackColor = System.Drawing.Color.Transparent;
+            this.picIcon.Location = new System.Drawing.Point(39, 133);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(100, 50);
+            this.picIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picIcon.TabIndex = 14;
+            this.picIcon.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(278, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Pressure";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picIcon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblpressure);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblwindspeed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblsunset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblsunrise);
+            this.Controls.Add(this.lblamanecer);
+            this.Controls.Add(this.lbldetalles);
+            this.Controls.Add(this.lblcondicion);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.txtcity);
+            this.Controls.Add(this.lbcity);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbcity;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Label lblcondicion;
+        private System.Windows.Forms.Label lbldetalles;
+        private System.Windows.Forms.Label lblamanecer;
+        private System.Windows.Forms.Label lblsunrise;
+        private System.Windows.Forms.Label lblsunset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblpressure;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblwindspeed;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox picIcon;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Domain/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using Main;
+
+namespace Domain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string APIKey = "5c61e63c6e9600c959150c6ca9326226";
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            GetWeather();
+        }
+         public void GetWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", txtcity.Text,APIKey);
+                var Json = web.DownloadString(url);
+                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(Json);
+
+
+                picIcon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                lblcondicion.Text = Info.weather[0].main;
+                lbldetalles.Text = Info.weather[0].description;
+                lblsunset.Text = Info.sys.sunset.ToString();
+                lblsunrise.Text = Info.sys.sunrise.ToString();
+                lblwindspeed.Text = Info.wind.speed.ToString();
+                lblpressure.Text = Info.main.preasure.ToString();
+
+            }
+        }
+    }
+}
